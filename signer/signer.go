34ee228a142583e0fb1bfa4df9402947ae5e0181
bynadezhda <n.malcova@gmail.com>
2025-12-27 23:27:46 +0300

    fix race condition

--- signer/signer.go
+++ signer/signer.go
@@ -7,11 +7,13 @@ import (
 	"sync"
 )
 
-var md5Lock = &sync.Mutex{}
+const hashCount = 6
+
+var md5Lock sync.Mutex
 
 func ExecutePipeline(jobs ...job) {
-	var in chan interface{} = nil
-	wg := &sync.WaitGroup{}
+	var in chan interface{}
+	var wg sync.WaitGroup
 
 	for _, j := range jobs {
 		out := make(chan interface{}, 10)
@@ -30,20 +32,25 @@ func ExecutePipeline(jobs ...job) {
 }
 
 func SingleHash(in, out chan interface{}) {
-	wg := &sync.WaitGroup{}
+	var wg sync.WaitGroup
 
 	for raw := range in {
-		data := strconv.Itoa((raw.(int)))
-		wg.Add(1)
+		val, ok := raw.(int)
+		if !ok {
+			continue
+		}
+		data := strconv.Itoa(val)
 
-		go func(raw interface{}, out chan interface{}) {
+		wg.Add(1)
+		go func(data string) {
 			defer wg.Done()
 
-			md5chan := make(chan string)
-			crc32chan := make(chan string)
-			crc32md5chan := make(chan string)
+			md5chan := make(chan string, 1)
+			crc32chan := make(chan string, 1)
+			crc32md5chan := make(chan string, 1)
 
 			go func() {
+				defer close(md5chan)
 				md5Lock.Lock()
 				md5 := DataSignerMd5(data)
 				md5Lock.Unlock()
@@ -51,29 +58,37 @@ func SingleHash(in, out chan interface{}) {
 			}()
 
 			go func() {
+				defer close(crc32chan)
 				crc32chan <- DataSignerCrc32(data)
 			}()
 
 			go func() {
+				defer close(crc32md5chan)
 				md5 := <-md5chan
 				crc32md5chan <- DataSignerCrc32(md5)
 			}()
 
-			out <- <-crc32chan + "~" + <-crc32md5chan
-		}(raw, out)
+			crc32 := <-crc32chan
+			crc32md5 := <-crc32md5chan
+			out <- crc32 + "~" + crc32md5
+		}(data)
 	}
 	wg.Wait()
 }
 
 func MultiHash(in, out chan interface{}) {
-	wg := &sync.WaitGroup{}
+	var wg sync.WaitGroup
 
 	for v := range in {
 		wg.Add(1)
-		go func(out chan interface{}, v interface{}) {
+		go func(val interface{}) {
 			defer wg.Done()
-			calculateCrc32Th(out, v.(string))
-		}(out, v)
+			s, ok := val.(string)
+			if !ok {
+				return
+			}
+			calculateCrc32Th(out, s)
+		}(v)
 	}
 
 	wg.Wait()
@@ -81,33 +96,31 @@ func MultiHash(in, out chan interface{}) {
 
 func calculateCrc32Th(out chan interface{}, data string) {
 	var wg sync.WaitGroup
-	hashes := make([]string, 6)
+	hashes := make([]string, hashCount)
 
-	for i := 0; i < 6; i++ {
+	for i := 0; i < hashCount; i++ {
 		wg.Add(1)
-		iter := i
 
 		go func() {
 			defer wg.Done()
-			hashes[iter] = DataSignerCrc32(strconv.Itoa(iter) + data)
+			hashes[i] = DataSignerCrc32(strconv.Itoa(i) + data)
 		}()
 	}
 
 	wg.Wait()
 
-	var b strings.Builder
-	for i := 0; i < 6; i++ {
-		b.WriteString(hashes[i])
-	}
-
-	out <- b.String()
+	out <- strings.Join(hashes, "")
 }
 
 func CombineResults(in, out chan interface{}) {
 	var results []string
 
 	for v := range in {
-		results = append(results, v.(string))
+		s, ok := v.(string)
+		if !ok {
+			continue
+		}
+		results = append(results, s)
 	}
 
 	sort.Strings(results)
